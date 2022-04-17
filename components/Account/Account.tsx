959b8ebc42ef1f8a91a1c5be206010230a09c350
Manoj Kelath <manojkelath1987@gmail.com>
2022-04-17 11:14:08 +0530

    make metamask and wallect connect work

--- components/Account/Account.tsx
+++ components/Account/Account.tsx
@@ -1,11 +1,12 @@
 import { useMoralis, useChain } from 'react-moralis';
 import { useTranslation } from 'react-i18next';
 import { getEllipsisTxt } from '../helpers/formatters';
-import { Button, Modal, ModalBody, ModalCloseButton, ModalContent, ModalFooter, ModalHeader, ModalOverlay, Text, useDisclosure } from '@chakra-ui/react';
+import { Box, Button, Image, Modal, ModalBody, ModalCloseButton, ModalContent, ModalFooter, ModalHeader, ModalOverlay, Stack, Text, useDisclosure } from '@chakra-ui/react';
 import { useState } from 'react';
-import Address from '../Address/Address';
+// import Address from '../Address/Address';
 import { getExplorer } from '../helpers/networks';
 import { connectors } from './config';
+
 
 function Account() {
   const { authenticate, isAuthenticated, account, chainId, logout } =
@@ -25,16 +26,15 @@ function Account() {
           <Modal isOpen={isOpen} onClose={onClose}>
             <ModalOverlay />
             <ModalContent>
-              <ModalHeader>Modal Title</ModalHeader>
               <ModalCloseButton />
               <ModalBody>
-                <div style={{ display: 'grid', gridTemplateColumns: '1fr 1fr' }}>
+                <Stack direction='row'>
                   {connectors.map(({ title, icon, connectorId }, key) => (
                     <div
                       key={key}
                       onClick={async () => {
                         try {
-                          await authenticate({ signingMessage: "CSSC Autentication" });
+                          await authenticate({ signingMessage: "CSSC Autentication", provider: connectorId }); // TODO: mention type
                           window.localStorage.setItem('connectorId', connectorId);
                           setIsAuthModalVisible(false);
                         } catch (e) {
@@ -42,11 +42,14 @@ function Account() {
                         }
                       }}
                     >
-                      <img src={icon} alt={title} />
+                      <Image boxSize='50px'
+                        objectFit='cover'
+                        src={icon} alt={title} />
                       <Text style={{ fontSize: '14px' }}>{title}</Text>
                     </div>
                   ))}
-                </div>
+                </Stack>
+
               </ModalBody>
 
 
--- components/Account/config.js
+++ components/Account/config.js
@@ -1,21 +1,15 @@
-import Metamask from "./WalletIcons/metamaskWallet.png";
-import Coin98 from "./WalletIcons/Coin98.png";
-import WalletConnect from "./WalletIcons/wallet-connect.svg";
-import MathWallet from "./WalletIcons/MathWallet.svg";
-import TokenPocket from "./WalletIcons/TokenPocket.svg";
-import SafePal from "./WalletIcons/SafePal.svg";
-import TrustWallet from "./WalletIcons/TrustWallet.png";
+
 
 export const connectors = [
   {
     title: "Metamask",
-    icon: Metamask,
+    icon: "/metamaskWallet.png",
     connectorId: "injected",
     priority: 1,
   },
   {
     title: "WalletConnect",
-    icon: WalletConnect,
+    icon: "/wallet-connect.svg",
     connectorId: "walletconnect",
     priority: 2,
   }

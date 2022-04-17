56872e017159408f997cf9f6623ddae60c7196b1
Manoj Kelath <manojkelath1987@gmail.com>
2022-04-17 10:25:46 +0530

    clean up accounts

--- components/Account/Account.tsx
+++ components/Account/Account.tsx
@@ -1,10 +1,9 @@
 import { useMoralis, useChain } from 'react-moralis';
 import { useTranslation } from 'react-i18next';
 import { getEllipsisTxt } from '../helpers/formatters';
-import { Button, Modal, Text, useDisclosure } from '@chakra-ui/react';
+import { Button, Modal, ModalBody, ModalCloseButton, ModalContent, ModalFooter, ModalHeader, ModalOverlay, Text, useDisclosure } from '@chakra-ui/react';
 import { useState } from 'react';
 import Address from '../Address/Address';
-// import { SelectOutlined } from '@ant-design/icons';
 import { getExplorer } from '../helpers/networks';
 import { connectors } from './config';
 
@@ -22,43 +21,36 @@ function Account() {
     return (
       <div className="connect-wal">
         <a>
-          <span onClick={onOpen}>
-            {t('connect_wallet')}
-          </span>
-          <Modal
-            isOpen={isOpen}
-            onClose={onClose}
-          >
-            <div
-              style={{
-                padding: '10px',
-                display: 'flex',
-                justifyContent: 'center',
-                fontWeight: '700',
-                fontSize: '20px',
-              }}
-            >
-              {'Connect Wallet'}
-            </div>
-            <div style={{ display: 'grid', gridTemplateColumns: '1fr 1fr' }}>
-              {connectors.map(({ title, icon, connectorId }, key) => (
-                <div
-                  key={key}
-                  onClick={async () => {
-                    try {
-                      await authenticate({ signingMessage: "CSSC Autentication" });
-                      window.localStorage.setItem('connectorId', connectorId);
-                      setIsAuthModalVisible(false);
-                    } catch (e) {
-                      console.error(e);
-                    }
-                  }}
-                >
-                  <img src={icon} alt={title} />
-                  <Text>{title}</Text>
+          <Button onClick={onOpen}>{'Connect Wallet'}</Button>
+          <Modal isOpen={isOpen} onClose={onClose}>
+            <ModalOverlay />
+            <ModalContent>
+              <ModalHeader>Modal Title</ModalHeader>
+              <ModalCloseButton />
+              <ModalBody>
+                <div style={{ display: 'grid', gridTemplateColumns: '1fr 1fr' }}>
+                  {connectors.map(({ title, icon, connectorId }, key) => (
+                    <div
+                      key={key}
+                      onClick={async () => {
+                        try {
+                          await authenticate({ signingMessage: "CSSC Autentication" });
+                          window.localStorage.setItem('connectorId', connectorId);
+                          setIsAuthModalVisible(false);
+                        } catch (e) {
+                          console.error(e);
+                        }
+                      }}
+                    >
+                      <img src={icon} alt={title} />
+                      <Text style={{ fontSize: '14px' }}>{title}</Text>
+                    </div>
+                  ))}
                 </div>
-              ))}
-            </div>
+              </ModalBody>
+
+
+            </ModalContent>
           </Modal>
         </a>
       </div>
